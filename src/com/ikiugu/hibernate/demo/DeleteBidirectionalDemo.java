250e41608b135206bc22b451a346ed0b2242ac97
Alfred Ikiugu <ikiugualf@gmail.com>
2020-02-07 13:29:13 +0300

    delete both from detail entity

---
+++ src/com/ikiugu/hibernate/demo/DeleteBidirectionalDemo.java
@@ -0,0 +1,45 @@
+package com.ikiugu.hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.ikiugu.hibernate.demo.entity.Instructor;
+import com.ikiugu.hibernate.demo.entity.InstructorDetail;
+
+public class DeleteBidirectionalDemo {
+
+	public static void main(String[] args) {
+		// create a factory
+		SessionFactory factory = new Configuration()
+				.configure()
+				.addAnnotatedClass(Instructor.class)
+				.addAnnotatedClass(InstructorDetail.class)
+				.buildSessionFactory();
+		
+		// create a session
+		Session session = factory.getCurrentSession();
+		
+		try {
+			//start a transaction
+			session.beginTransaction();
+			
+			//get instructor with id
+			InstructorDetail instructorDetail = session.get(InstructorDetail.class, 3);
+			
+			//delete the instructor
+			if (instructorDetail != null) {
+				session.delete(instructorDetail);
+			}
+			
+			//commit a transaction
+			session.getTransaction().commit();
+			
+		} catch(Exception e){
+			System.out.println("Exception thrown: " + e.getMessage());
+		} finally {
+			factory.close();
+		}
+	}
+	
+}
